542ccaaf0abb68a3dcc6fdd9526c2d52115f6be3
Bruno <brunolb.lafont@gmail.com>
2023-05-04 16:29:54 +0200

    add test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 import { createDoing } from "./doingDiv.js";
 import { creatodo } from "./tododiv.js";
 
-
 const DoingBtn = document.querySelector("#doing__button");
 DoingBtn.addEventListener("click", createDoing);
 const buttontodoDiv = document.querySelector("#to_do__button");
-buttontodoDiv.addEventListener("click",creatodo);
-
-
-
-
+buttontodoDiv.addEventListener("click", creatodo);
+test;
