dc3416b67ec17f1e535a4f7d54ebe11f025efc32
Bruno <brunolb.lafont@gmail.com>
2023-05-04 14:27:38 +0200

    add list button

--- assets/js/doingDiv.js
+++ assets/js/doingDiv.js
@@ -1,18 +1,39 @@
+let i = 0;
+
 export function createDoing() {
   const doingDiv = document.querySelector(".doing");
   const createDiv = document.createElement("div");
-  createDiv.classList.add("doing__div");
+  createDiv.classList.add("doing__div" + i);
   doingDiv.appendChild(createDiv);
   const createDivInputText = document.createElement("input");
   createDivInputText.setAttribute("type", "text");
   createDivInputText.classList.add("doing__div__input_text");
-  doingDiv.appendChild(createDivInputText);
+  createDiv.appendChild(createDivInputText);
   const createDivInputBtn = document.createElement("input");
   createDivInputBtn.setAttribute("type", "button");
   createDivInputBtn.setAttribute("value", "Add");
   createDivInputBtn.classList.add("doing__div__input_btn");
-  doingDiv.appendChild(createDivInputBtn);
+  createDiv.appendChild(createDivInputBtn);
   const createDivUl = document.createElement("ul");
   createDivUl.classList.add("doing__div__ul");
-  doingDiv.appendChild(createDivUl);
+  createDiv.appendChild(createDivUl);
+
+  // Fix 1: Move addTextUl function outside createDoing function
+  // to avoid creating multiple event listeners on the button
+  function addTextUl() {
+    const createDivUl = createDiv.querySelector(".doing__div__ul"); // get the ul in the current div
+    const createDivInputText = createDiv.querySelector(
+      ".doing__div__input_text"
+    ); // get the input text in the current div
+    const createDivLi = document.createElement("li");
+    createDivLi.innerText = createDivInputText.value;
+    createDivUl.appendChild(createDivLi);
+    createDivInputText.value = ""; // clear the input field
+  }
+
+  // Fix 2: Increment i after assigning class to the div
+  i++;
+
+  // Fix 3: Add event listener to the button outside addTextUl function
+  createDivInputBtn.addEventListener("click", addTextUl);
 }
--- assets/js/doingDivText.js
+++ assets/js/doingDivText.js
@@ -1,11 +1,8 @@
-let array = [];
-export function exportTextDoing() {
-  const divInpuText = document.querySelector(".doing__div__input_text");
-  let text = divInpuText.value;
-  const doingDivUl = document.querySelector(".doing__div__ul");
-  const doigDivLi = document.createElement("li");
-  doingDivUl.appendChild(doigDivLi);
-  doigDivLi.textContent = text;
-  array.push(text);
-  divInpuText.value = "";
+export function addTextUl() {
+  const createDivUl = createDiv.querySelector(".doing__div__ul"); // get the ul in the current div
+  const createDivInputText = createDiv.querySelector(".doing__div__input_text"); // get the input text in the current div
+  const createDivLi = document.createElement("li");
+  createDivLi.innerText = createDivInputText.value;
+  createDivUl.appendChild(createDivLi);
+  createDivInputText.value = ""; // clear the input field
 }
