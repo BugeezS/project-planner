e83a402e2905c6dc146e148525b4c0a0adb0ab73
Ismaël Mesmoudi Atou <ismaelmesmoudiatou@gmail.com>
2023-05-04 16:17:50 +0200

    add tododiv

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+
+import { creatodo } from "./tododiv.js";
+// import { exportTextTodo } from "./tododivtext.js";
+
+
+const buttontodoDiv = document.querySelector("#to_do__button");
+buttontodoDiv.addEventListener("click",creatodo);
+
+
+
---
+++ assets/js/tododivtext.js
@@ -0,0 +1,11 @@
+let arraytodo = [];
+export function exportTextTodo() {
+    const divInputText = document.querySelector(".to_do__div__input_text");
+    let text = divInputText.value;
+    const todoDivUl = document.querySelector(".to_do__div__ul");
+    const todoDivLi = document.createElement("li");
+    todoDivUl.appendChild(todoDivLi);
+    todoDivLi.textContent = text;
+    arraytodo.push(text);
+    divInputText.value = "";
+} 
